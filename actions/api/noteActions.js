ce004a1d70d1ca79f465f4cd9f930b74e1035ac6
sebix00 <you@example.com>
2021-12-11 13:56:14 +0100

    save, update, get Notes

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -1,37 +1,63 @@
 const Note = require("../../db/models/note")
 
 class NoteActions {
-   saveNote(req,res){
-//     const newNote = new Note({
-//         title:"zakupy",
-//         body: "jajka",
-//     })
+  async saveNote(req,res){
+       
+    const title = req.body.title;
+    const body = req.body.body
+    let note;
+    try{
+        note = new Note({title,body})
+        await note.save();
+
+    }catch(err){
+        return res.status(422).json({message:err.message});
+    }
+
     
-//     newNote.save().then(()=>{
-//         console.log("Notatka nozstala zapisana")
-//     })
 
-const title = req.body.title;
-const body = req.body.body
+       res.status(201).json(note);
+   }
+   //pobieranie notatek
+  async getAllNotes(req,res){
+      let doc;
+      try{
+         doc = await Note.find({});
+          
+      }catch(err){
+         return res.status(500).json({message:err.message})
+      }
+    
+    console.log(doc)
+    res.status(200).json(doc)
+}
 
-       res.send(`notatka zostala stworzona tytul: ${title}, tresc: ${body}`)
+//pobieranie konkretnej notatki
+   async getNote(req,res){
+       const id = req.params.id;
+       const note = await Note.findOne({_id: id});
+
+       res.status(200).json(note);
+    }
+    //aktualizacja notatki
+  async updateNote(req,res){
+
+       const title = req.body.title;
+       const body = req.body.body
+       const id = req.params.id;
+
+    const note =  await Note.findOne({_id:id});
+    note.title=title;
+    note.body=body;
+    await note.save()
+       res.status(201).json(note);
    }
-   getAllNotes(req,res){
-       //pobieranie notatek
-       res.send("API dziala")
-   }
-   getNote(req,res){
-       //pobieranie konkretnej notatki
-       res.send("Info o notatce")
-   }
-   updateNote(req,res){
-       //aktualizacja notatki
-       res.send("notatka zaktualizowana")
-   }
-   deleteNote(req,res){
+   //usuwanie notatki
+  async deleteNote(req,res){
        const id = req.params.id
-       //usuwanie notatki
-       res.send(`Notatka usunieta ID: ${id}`)
+       await Note.deleteOne({_id:id});
+       
+       res.sendStatus(204);
    }
 }
 
--- db/models/note.js
+++ db/models/note.js
@@ -1,7 +1,15 @@
 const mongoose =require("mongoose");
-const Note = mongoose.model("Note",{
-    title:String,
-    body:String
+const NoteSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    body:{
+        type:String,
+        required:true,
+    }
+
 })
+const Note = mongoose.model("Note",NoteSchema)
 
 module.exports = Note;
